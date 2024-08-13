46ae6dbc5fbb127b51de6ea168cb1e8bb5c9020d
N V ANAND SAI KUMAR <104138656+Narla-Venkata-Anand-Sai-Kumar@users.noreply.github.com>
2024-08-13 13:52:30 +0530

    Update ip_camera.py

--- nodes/ip_camera.py
+++ nodes/ip_camera.py
@@ -1,41 +1,39 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 import cv2
 import argparse
-import roslib
 import rospy
-from sensor_msgs.msg import Image 
-from std_msgs.msg import String
-from cv_bridge import CvBridge, CvBridgeError 
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
 
 class IPCamera(object):
     def __init__(self, url):
         try:
             self.vcap = cv2.VideoCapture(url)
-        except:
-            rospy.logerr('Unable to open ip camera stream: ' + str(url))
+        except Exception as e:
+            rospy.logerr(f'Unable to open IP camera stream: {url}. Error: {e}')
             sys.exit()
         self.image_pub = rospy.Publisher("/camera/image_raw", Image, queue_size=10)
         self.bridge = CvBridge()
 
 if __name__ == '__main__':
     # Usage:
-    # $ python ip_camera -u YOUR_IP_CAMERA_URL -g
-    parser = argparse.ArgumentParser(prog='ip_camera.py', description='reads a given url string and dumps it to a ros_image topic')
-    parser.add_argument('-g', '--gui', action='store_true', help='show a GUI of the camera stream')
-    parser.add_argument('-u', '--url', default='http://192.168.43.1:8080/video', help='camera stream url to parse')
-    args = parser.parse_args(rospy.myargv()[1:])
+    # $ python3 ip_camera.py -u YOUR_IP_CAMERA_URL -g
+    parser = argparse.ArgumentParser(prog='ip_camera.py', description='Reads a given URL string and dumps it to a ROS image topic')
+    parser.add_argument('-g', '--gui', action='store_true', help='Show a GUI of the camera stream')
+    parser.add_argument('-u', '--url', default='http://192.168.43.1:8080/video', help='Camera stream URL to parse')
+    args = parser.parse_args(rospy.myargv(argv=sys.argv[1:]))
     
     rospy.init_node('ip_camera', anonymous=True)
     
-    print 'Opening ip camera'
+    print('Opening IP camera')
     ip_camera = IPCamera(args.url)
-    print 'Successfully opened ip camera'
+    print('Successfully opened IP camera')
     
     while not rospy.is_shutdown() and ip_camera.vcap.isOpened():
         ret, frame = ip_camera.vcap.read()
-        if ret == False:
-            print 'Could not read frame'
+        if not ret:
+            print('Could not read frame')
             break
         
         img_msg = ip_camera.bridge.cv2_to_imgmsg(frame, "bgr8")
@@ -44,7 +42,7 @@ if __name__ == '__main__':
         
         if args.gui:
             cv2.imshow('IP Camera', frame)
-            if cv2.waitKey(1) == 27:
+            if cv2.waitKey(1) == 27:  # ESC key to break
                 break
 
     ip_camera.vcap.release()
